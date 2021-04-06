6ab674e2ee12a13377918f1d93962b3c252698c3
sanjana k <sanjanarthi@gmail.com>
2021-04-06 19:55:53 +0530

    Add project files.

---
+++ Day23_CabInvoiceGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day23_CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+
+}
+
